d8674e6e1edc1f5006f714b51bed019bc30daf45
Nikola Hadzic <nikola.hadzic@proton.me>
2025-12-08 13:00:09 +0100

    format

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -8,18 +8,15 @@ export const routes: Routes = [
     children: [
       {
         path: 'today',
-        loadComponent: () =>
-          import('../today/today.page').then((m) => m.TodayPage),
+        loadComponent: () => import('../today/today.page').then((m) => m.TodayPage),
       },
       {
         path: 'statistics',
-        loadComponent: () =>
-          import('../statistics/statistics.page').then((m) => m.StatisticsPage),
+        loadComponent: () => import('../statistics/statistics.page').then((m) => m.StatisticsPage),
       },
       {
         path: 'settings',
-        loadComponent: () =>
-          import('../settings/settings.page').then((m) => m.SettingsPage),
+        loadComponent: () => import('../settings/settings.page').then((m) => m.SettingsPage),
       },
       {
         path: '',
