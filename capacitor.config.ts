aaf34c9583c4e4922aac89c5cd1f0fdcddf05487
Nikola Hadzic <nikola.hadzic@usb.ch>
2025-12-09 08:52:12 +0100

    change name to habix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.habitify.app',
-  appName: 'Habitify',
+  appId: 'com.habix.app',
+  appName: 'Habix',
   webDir: 'www',
 };
 
